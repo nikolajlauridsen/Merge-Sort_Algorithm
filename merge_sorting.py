3bc1c3034ef87c039bf2d3f19eb5da41f41b9f73
Nikolaj Lauridsen <nikolajlauridsen@protonmail.ch>
2016-11-02 16:49:48 +0100

    Add comments

--- merge_sorting.py
+++ merge_sorting.py
@@ -2,18 +2,29 @@ def mergesort(a):
     if len(a) == 1:
         return a
 
+    # Split the 'a' array into two equally sized arrays
     li_1 = a[:len(a)//2]
     li_2 = a[len(a)//2:]
 
+    # Split the new arrays again until there's
+    # only 1 item in each of the two arrays
     li_1 = mergesort(li_1)
     li_2 = mergesort(li_2)
 
+    # Start merging arrays until every array "fragment" is merged again
     return merge(li_1, li_2)
 
+
 def merge(a, b):
+    # Resulting sorted array
     c = []
+    # We have to keep track of array length our self since it doesn't
+    # update through each loop iteration
     length_a = len(a)
     length_b = len(b)
+
+    # As long as both arrays has items we'll add the smallest item to
+    # the result array 'c' and then delete it from where it came
     while length_a > 0 and length_b > 0:
         if a[0] > b[0]:
             c.append(b[0])
@@ -24,6 +35,8 @@ def merge(a, b):
             del a[0]
             length_a -=1
 
+    # Dump the rest of the items to the end of the result array
+    # if there's more than one it'll be sorted on the next run through
     while length_a > 0:
         c.append(a[0])
         del a[0]
@@ -36,7 +49,7 @@ def merge(a, b):
 
     return c
 
-numbers = [4,1,7,8,2,9,10,11,11,255,20,1,6]
+numbers = [15,6,30,100,-5,-3.5,24.252, 24.251]
 print(numbers)
 numbers = mergesort(numbers)
 print(numbers)
